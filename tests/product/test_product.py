0231241e2e967f6c73ce2ed9fc1228b0bc2beaf2
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-12 16:18:06 -0300

    feat(requisito-1): adiciona requisito completo.

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        nome_do_produto="Celular",
+        nome_da_empresa="Xaomi",
+        data_de_fabricacao="22/08/2010",
+        data_de_validade="10/11/2022",
+        numero_de_serie="123456",
+        instrucoes_de_armazenamento="armazenamento interno",
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Celular"
+    assert product.nome_da_empresa == "Xaomi"
+    assert product.data_de_fabricacao == "22/08/2010"
+    assert product.data_de_validade == "10/11/2022"
+    assert product.numero_de_serie == "123456"
+    assert product.instrucoes_de_armazenamento == "armazenamento interno"
