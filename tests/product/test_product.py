1c6b7001da81815cdbadc7d911fe00aa73fb315f
Pedro Lima <pedro.oficial1969@gmail.com>
2022-08-12 16:47:24 -0300

    feat(requisito-2): adiciona requisito completo.

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -3,13 +3,13 @@ from inventory_report.inventory.product import Product
 
 def test_cria_produto():
     product = Product(
-        1,
+        id=1,
         nome_do_produto="Celular",
         nome_da_empresa="Xaomi",
         data_de_fabricacao="22/08/2010",
         data_de_validade="10/11/2022",
         numero_de_serie="123456",
-        instrucoes_de_armazenamento="armazenamento interno",
+        instrucoes_de_armazenamento="livremente",
     )
 
     assert product.id == 1
@@ -18,4 +18,4 @@ def test_cria_produto():
     assert product.data_de_fabricacao == "22/08/2010"
     assert product.data_de_validade == "10/11/2022"
     assert product.numero_de_serie == "123456"
-    assert product.instrucoes_de_armazenamento == "armazenamento interno"
+    assert product.instrucoes_de_armazenamento == "livremente"
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="Celular",
+        nome_da_empresa="Xaomi",
+        data_de_fabricacao="22/08/2010",
+        data_de_validade="10/11/2022",
+        numero_de_serie="123456",
+        instrucoes_de_armazenamento="livremente",
+    )
+
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+)
